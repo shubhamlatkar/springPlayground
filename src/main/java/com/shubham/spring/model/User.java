b2dda106659a8d6a0d71457c310a4eb21b7f0e7b
shubham-latkar <shubhamlatkar@yahoo.co.in>
2020-07-04 11:18:58 +0000

    Spring with mongodb

---
+++ src/main/java/com/shubham/spring/model/User.java
@@ -0,0 +1,17 @@
+package com.shubham.spring.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document
+@Data
+@NoArgsConstructor
+public class User {
+    @Id
+    private int userId;
+    private String userName;
+    private String password;
+}
