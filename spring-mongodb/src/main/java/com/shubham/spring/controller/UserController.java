07334933c9c816b5e97fc60c601af4eed8124157
shubhamlatkar <shubhamlatkar@gmail.com>
2020-07-06 16:26:11 +0000

    adding spring security

--- spring-mongodb/src/main/java/com/shubham/spring/controller/UserController.java
+++ spring-mongodb/src/main/java/com/shubham/spring/controller/UserController.java
@@ -1,15 +1,14 @@
 package com.shubham.spring.controller;
 
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 import com.shubham.spring.document.Users;
 import com.shubham.spring.repository.UserRepository;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
---
+++ spring-security/src/main/java/com/shubham/springSecurity/SpringBootSecurityDemo.java
@@ -0,0 +1,13 @@
+package com.shubham.springSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityDemo {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityDemo.class, args);
+	}
+
+}
