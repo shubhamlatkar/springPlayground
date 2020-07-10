2633688b8f07304f8ba18afb381362f59d9484f5
shubham latkar <shbhamlatkar@gmail.com>
2020-07-10 12:14:41 +0530

    addeing db auth

---
+++ security-DBAuth/src/main/java/com/shubham/SpringSecurity/auth/ApplicationUser.java
@@ -0,0 +1,65 @@
+package com.shubham.SpringSecurity.auth;
+
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class ApplicationUser implements UserDetails {
+
+    private final List<? extends GrantedAuthority> grantedAuthorities;
+    private final String password;
+    private final String username;
+    private final Boolean isAccountNonExpired;
+    private final Boolean isAccountNonLocked;
+    private final Boolean isCredentialsNonExpired;
+    private final Boolean isEnabled;
+
+
+    public ApplicationUser(List<? extends GrantedAuthority> grantedAuthorities, String password, String username, Boolean isAccountNonExpired, Boolean isAccountNonLocked, Boolean isCredentialsNonExpired, Boolean isEnabled) {
+        this.grantedAuthorities = grantedAuthorities;
+        this.password = password;
+        this.username = username;
+        this.isAccountNonExpired = isAccountNonExpired;
+        this.isAccountNonLocked = isAccountNonLocked;
+        this.isCredentialsNonExpired = isCredentialsNonExpired;
+        this.isEnabled = isEnabled;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
