938711a89a4e6f1371197fd8d99c665344946abd
shubhamlatkar <shubhamlatkar@gmail.com>
2020-07-17 01:01:09 +0530

    added api gateway

--- spring-cloud/order-service/src/main/java/com/springCloud/order/controller/OrderController.java
+++ spring-cloud/order-service/src/main/java/com/springCloud/order/controller/OrderController.java
@@ -5,12 +5,14 @@ import com.springCloud.order.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@RequestMapping("/order")
 public class OrderController {
 
     private final OrderRepository orderRepository;
--- spring-cloud/user-service/src/main/java/com/springCloud/user/controller/UserController.java
+++ spring-cloud/user-service/src/main/java/com/springCloud/user/controller/UserController.java
@@ -5,12 +5,14 @@ import com.springCloud.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
     private final UserRepository userRepository;
