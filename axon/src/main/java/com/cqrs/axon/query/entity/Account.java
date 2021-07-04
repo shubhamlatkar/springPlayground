da5f0a5fea1949ec4568195eedcb5872bd2e0c3d
shubhamlatkar <shubhamlatkar@gmail.com>
2021-07-04 15:33:09 +0530

    added lombok annotation

--- axon/src/main/java/com/cqrs/axon/query/entity/Account.java
+++ axon/src/main/java/com/cqrs/axon/query/entity/Account.java
@@ -1,38 +1,17 @@
 package com.cqrs.axon.query.entity;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.math.BigDecimal;
 
 @Entity
+@Data
 public class Account {
 
     @Id
     private String accountId;
     private BigDecimal balance;
     private String status;
-
-    public String getAccountId() {
-        return accountId;
-    }
-
-    public void setAccountId(String accountId) {
-        this.accountId = accountId;
-    }
-
-    public BigDecimal getBalance() {
-        return balance;
-    }
-
-    public void setBalance(BigDecimal balance) {
-        this.balance = balance;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
 }
