4c67e10f8ca63145b19901f78632626baf863f06
shubhamlatkar <shubhamlatkar@gmail.com>
2021-07-12 22:55:28 +0530

    adding sample frontend code to be renderd from spring app

---
+++ frontend/src/main/frontend/src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Spring</h1>
+      <h2>Build generated through docker!</h2>
+    </div>
+  );
+}
